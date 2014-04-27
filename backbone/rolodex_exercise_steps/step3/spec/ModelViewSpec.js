d04a5dcf136acdb0565087159c8ba168a946dc5a
brendajin <brendajin@gmail.com>
2014-04-27 10:24:14 -0700

    all specs in all exercises

--- backbone/rolodex_exercise_steps/step3/spec/ModelViewSpec.js
+++ backbone/rolodex_exercise_steps/step3/spec/ModelViewSpec.js
@@ -3,8 +3,14 @@ describe("Exercise 3: ", function() {
     var personView;
 
     beforeEach(function() {
+      var person = new Person({firstName: "Grace",
+        lastName: "Hopper",
+        role: "Computer Scientist",
+        imgUrl: "http://www.history.navy.mil/photos/images/h96000/h96920k.jpg"
+      })
+
       personView = new PersonView({
-        model: model
+        model: person
       });
     });
 
@@ -16,26 +22,23 @@ describe("Exercise 3: ", function() {
       expect(personView.$el.hasClass('rolodex')).toBe(true);
     });
 
-    it("give your PersonView a className of 'thumbnail'", function() {
-      expect(personView.$el.hasClass('thumbnail')).toBe(true);
-    });
-
-    it("give your PersonView a .render method that gets called upon instantiation", function() {
-      spyOn(personView, 'render');
-      personView.initialize();
-      expect(personView.render).toHaveBeenCalled();
+    it("give your PersonView a .render method", function() {
+      expect(personView.render).toBeDefined();
     });
 
     it("in .render, use jQuery to add an img tag to the $el", function() {
+      personView.render();
       expect(personView.$('img').length).toBeGreaterThan(0);
     })
 
     it("in .render, use jQuery to add an src attribute to the img", function() {
+      personView.render();
       expect(personView.$('img').attr('src')).toBeDefined();
     });
 
     it("use model.get('imgUrl') to populate the src tag", function() {
-      expect(personView.$('img').attr('src')).toEqual(model.get('imgUrl'));
+      personView.render();
+      expect(personView.$('img').attr('src')).toEqual(personView.model.get('imgUrl'));
     })
 
     it("make sure your .render function returns this", function() {
@@ -55,7 +58,7 @@ describe("Exercise 3: ", function() {
 
     it("make sure that 'onClick' is defined as a method on the PersonView", function() {
       expect(personView.onClick).toBeDefined();
-      $('body').append(personView.$el);
+      $('body').append(personView.render().$el);
       spyOn(personView, 'onClick');
       personView.delegateEvents();
       $('img').click();
@@ -65,21 +68,31 @@ describe("Exercise 3: ", function() {
   });
 
   describe("Instantiate a new PersonView: ", function() {
+    beforeEach(function() {
+      var person = new Person({firstName: "Grace",
+        lastName: "Hopper",
+        role: "Computer Scientist",
+        imgUrl: "http://www.history.navy.mil/photos/images/h96000/h96920k.jpg"
+      })
+
+      personView = new PersonView({
+        model: person
+      });
+    });
+
     it("store it as a variable called 'personView'", function() {
       expect(personView).toBeDefined();
     });
 
-    it("give your instantiated view a model that you previously. You can either use the one provided, or create a new one. If you make a new model, make sure it has an 'imgUrl' attribute", function() {
+    it("give your instantiated view a model. Make sure the model has an 'imgUrl' attribute", function() {
       expect(personView.model).toBeDefined();
       expect(personView.model.get('imgUrl')).toBeDefined();
     });
   });
 
   describe("In app.js, listen for $(document).ready(): ", function() {
-
     it("and when the document is ready, append your personView to the DOM", function() {
-      var myNode = $('img.thumbnail');
-      expect(myNode.length).toBeGreaterThan(0);
+      expect($('body').children('.rolodex').length).toBeGreaterThan(0);
     });
   });
 });
--- backbone/rolodex_exercise_steps/step4/spec/CollectionViewSpec.js
+++ backbone/rolodex_exercise_steps/step4/spec/CollectionViewSpec.js
@@ -7,9 +7,9 @@ describe("Exercise 4: ", function() {
       $('body').append($('<script id="rolodex-template" type="text/x-handlebars-template">'));
       var source   = $("#rolodex-template").html();
       var template = Handlebars.compile(source);
-      
+
       myView = new RolodexView({
-        collection: collection
+        collection: people
       });
     });
 
@@ -18,7 +18,7 @@ describe("Exercise 4: ", function() {
     })
 
     it("store it as a variable called 'RolodexView'", function() {
-      expect(View).not.toBeUndefined();
+      expect(RolodexView).not.toBeUndefined();
     });
 
     it("define a .render function", function() {
@@ -28,32 +28,6 @@ describe("Exercise 4: ", function() {
     it("make sure your .render function returns this", function() {
       var returnExpectation = myView.render();
       expect(returnExpectation).toEqual(myView);
-    });
-
-    it("inside of your .render function, compile a Handlebars template", function() {
-      spyOn(Handlebars, 'compile');
-      myView.render();
-      expect(Handlebars.compile).toHaveBeenCalled();
-      expect(Handlebars.compile).toHaveBeenCalledWith($("#rolodex-template").html());
-    })
-
-    it("inside of your .render function, pass JSON into a Handlebars template using collection.toJSON", function() {
-      expect(template(collection.toJSON())).not.toThrow();
-    });
-
-    it("inside of your .render function, append the rendered template to your $el", function() {
-      expect(myView.$el.children().length).toBeGreaterThan(0);
-    })
-
-    it("inside of your .render function, don't forget to return this", function() {
-      var returnExpectation = myView.render();
-      expect(returnExpectation).toEqual(myView);
-    })
-
-    it("in .initialize, create an event listener for the 'reset' event with a callback of .render", function() {
-      spyOn(myView,'render');
-      myView.collection.trigger('reset');
-      expect(myView.render).toHaveBeenCalled();
     });
 
   });
