f07e43883416f94199ebde4ba778465516dff817
Brenda Jin <brendajin@gmail.com>
2014-08-16 18:59:52 -0700

    gave instructions on paths

--- backbone/rolodex_exercise_steps/step0/spec/exampleSpec.js
+++ backbone/rolodex_exercise_steps/step0/spec/exampleSpec.js
@@ -1,4 +1,4 @@
-describe("define a global variable called areGirlDevelopersCool", function() {
+describe("In src/app.js, define a global variable called areGirlDevelopersCool", function() {
     it("and set it to true", function() {
 	    expect(areGirlDevelopersCool).toBeDefined();
 	    expect(areGirlDevelopersCool).toEqual(true);
