f6e9f6ab62bab02575fa2b8605e439502e650785
claire <cdersky@gmail.com>
2016-07-08 12:24:53 -0700

    update the cashRegister solution and assingment

--- javascript/exercises/cashRegisterSolution.js
+++ javascript/exercises/cashRegisterSolution.js
@@ -1,25 +1,12 @@
-var carts = {
-  brenda: { 
-    banana: "1.25",
-    handkerchief: ".99",
-    Tshirt: "25.01"
-    },
-  claire: {
-    "apple": "0.60",
-    "nalgene": "10.34",
-    "protein_shake": "22.36"
-    }
-};
-
-
 function cashRegister(cart){
-  var people = Object.keys(cart); // array of the carts keys, which are people  
-  var items = []; // array of the each people keys, which are items  
+  var people = Object.keys(cart); // array of people  
+  var cartItems = []; // array of items in each person's cart.
   var result = [];
   
   for (var i = 0; i < people.length; i++) {
-    // for each person grab the every item
-    items[i] = Object.keys(cart[people[i]]); 
+    // for each person grab all item
+    cartItems[i] = Object.keys(cart[people[i]]);
+    // Note, this makes cart items a nested array (ie [[ 'banana', 'blueberry']['peacans', 'pistacio']] ). The array of items at index i belongs to the person at index i in the people array. 
   }
 
   var i = 0;
@@ -29,16 +16,16 @@ function cashRegister(cart){
   while(i < people.length){
   // grab the price of every item for a particular person 
     // person is represented by cart[people[i]]
-    // items held by a person are items[i][j]
+    // cartItems held by a person are cartItems[i][j]
   //and add to the total
-    sum += Number(cart[people[i]][items[i][j]]); // this doesn't work because we have the same currency problem (removing the $)
+    sum += Number(cart[people[i]][cartItems[i][j]]); 
 
-    if(j < items[i].length -1){
+    if(j < cartItems[i].length - 1){
       j++;
     } else {
       j = 0;
+      result.push([people[i], sum]);
       i++;
-      result.push(sum);
       sum = 0;
     }
   }
@@ -48,4 +35,17 @@ function cashRegister(cart){
 
 
 /************* test ************/
-console.log(cashRegister(carts)); //[ 27.25, 33.3 ]
+var carts = {
+  brenda: { 
+    banana: "1.25",
+    handkerchief: ".99",
+    Tshirt: "25.01"
+    },
+  claire: {
+    apple: "0.60",
+    nalgene: "10.34",
+    proteinShake: "22.36"
+    }
+};
+
+console.log(cashRegister(carts)); //[ [ 'brenda', 27.25 ], [ 'claire', 33.3 ] ]
